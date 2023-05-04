907ac12795a3684ee981cc2d236283d9cc0e5a3f
MannShah17 <mannshah2004@gmail.com>
2023-05-05 01:32:05 +0530

    done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,9 +5,10 @@ const bcrypt = require('bcrypt');
 const registerUser = async (req, res) => {
   try {
     // 1) Hash the password of a user using bcrypt js
-    // 2) Check if the confirmpassword mathes the password
-    // 3) If the confirmpassword===password then store the document of a user
-    // 4) If the confirmpassword!=password then status should be 404 with the message 'Incorect Data'
+    // 2) Check if the confirmpassword matches the password
+    // 3) check for existing user if the email id already exists than show a json message User already exists
+    // 4) If the confirmpassword===password then store the document of a user
+    // 5) If the confirmpassword!=password then status should be 400 with the message: Passwords do not match
   } catch (error) {
     res.status(404).send(error);
   }
@@ -18,37 +19,25 @@ const loginUser = async (req, res) => {
   try {
     // 1) For login user will provide email and password
     // 2) check if the user exists in the database
-    // 3) Also check if the password mathes with the hashed password of the database
-    // 4) If user email exists and password matches with the db then show res.send('successful Login')
-    // 5) If the user Data is incorrect show res.send('Error Details Incorrect')
+    // 3) Also check if the password matches with the hashed password of the database
+    // 4) If user email exists and password matches with the db then show a json message Successful login
+    // 5) If the user Data is incorrect show a json message Invalid credentials
   } catch (error) {
     res.status(404).send(error);
   }
 };
 
-// Logout User
-const logoutUser = async (req, res) => {
-  try {
-    // 1) Using req.params.id require that document id which needs to be deleted
-    // 2) Check if the email mathes with that id document
-    // 3) If the email mathes than delete that document of a user and send ('User Deleted')
-    // 4) If error occurs while deleting user show Error deleting User
-  } catch (error) {
-    res.status(404).json(error);
-  }
-};
-
 //Updating Password
-const updatePasword = async (req, res) => {
+const updatePassword = async (req, res) => {
   try {
     // 1) Using req.params.id require that document id which password needs to be Updated
     // 2) Check if the email===req.body.email
-    // 3) If the db email mathes the req.body.email than using findByIdAndUpdate update old password with the new password
+    // 3) If the db email mathes the req.body.email than using findByIdAndUpdate update old password with the new password remember to hash password
     // 4) If the password updated successfully then show updated password as a result
-    // 5) If the email is not valid then show res.send('Email not valid')
+    // 5) If the email is not valid then show res.send('Invalid email') with status 400
   } catch (error) {
     res.status(404).json(error);
   }
 };
 
-module.exports = { registerUser, loginUser, logoutUser, updatePasword };
+module.exports = { registerUser, loginUser, logoutUser, updatePassword };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,14 +4,11 @@ const router = express.Router();
 const {
   registerUser,
   loginUser,
-  logoutUser,
-  updatePasword,
+  updatePassword,
 } = require('../controllers/userControllers');
-const { update } = require('../models/usermodel');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-router.delete('/logout/:id', logoutUser);
-router.put('/update/:id', updatePasword);
+router.put('/update/:id', updatePassword);
 
 module.exports = router;
